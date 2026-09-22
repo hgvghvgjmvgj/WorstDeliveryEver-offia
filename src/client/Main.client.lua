52ca7320d22dd564a712db98eaff679ff4dcfe6c
hgvghvgjmvgj <130692440+hgvghvgjmvgj@users.noreply.github.com>
2026-09-22 08:41:14 +0300

    M2: start local bay presentation

--- src/client/Main.client.lua
+++ src/client/Main.client.lua
@@ -6,10 +6,12 @@ local InteractionController = require(Controllers:WaitForChild("InteractionContr
 local PrototypeUIController = require(Controllers:WaitForChild("PrototypeUIController"))
 local CameraProtectionController = require(Controllers:WaitForChild("CameraProtectionController"))
 local FeelFeedbackController = require(Controllers:WaitForChild("FeelFeedbackController"))
+local BayController = require(Controllers:WaitForChild("BayController"))
 
 PrototypeUIController.Start()
 InteractionController.Start(PrototypeUIController)
 CameraProtectionController.Start()
 FeelFeedbackController.Start()
+BayController.Start()
 
-print("[ONE TRIP] M1.1 client loaded")
+print("[ONE TRIP] M2 client loaded")
