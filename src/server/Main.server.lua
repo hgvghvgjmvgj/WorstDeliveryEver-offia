41dad16e72f2fbb229aecb2cf4ad3227ec197ade
hgvghvgjmvgj <130692440+hgvghvgjmvgj@users.noreply.github.com>
2026-09-22 07:50:10 +0300

    M1: start carry prototype services

--- src/server/Main.server.lua
+++ src/server/Main.server.lua
@@ -3,10 +3,20 @@
 local ServerScriptService = game:GetService("ServerScriptService")
 
 local Services = ServerScriptService:WaitForChild("Services")
+
 local RemoteService = require(Services:WaitForChild("RemoteService"))
 local WorldService = require(Services:WaitForChild("WorldService"))
+local CollisionService = require(Services:WaitForChild("CollisionService"))
+local ItemService = require(Services:WaitForChild("ItemService"))
+local CarryService = require(Services:WaitForChild("CarryService"))
+local UnloadService = require(Services:WaitForChild("UnloadService"))
 
 RemoteService.Initialize()
-WorldService.Build()
+CollisionService.Start()
 
-print("[ONE TRIP] M0 foundation loaded")
+local world = WorldService.Build()
+ItemService.Start(world)
+CarryService.Start(ItemService)
+UnloadService.Start(world, CarryService)
+
+print("[ONE TRIP] M1 core carry prototype loaded")
