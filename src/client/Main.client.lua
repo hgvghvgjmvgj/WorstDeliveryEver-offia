b4e1a3cfaa0f9761b1bb6be20dbd0a4cc7b93e53
hgvghvgjmvgj <130692440+hgvghvgjmvgj@users.noreply.github.com>
2026-09-22 08:07:25 +0300

    M1.1: start feel feedback controller

--- src/client/Main.client.lua
+++ src/client/Main.client.lua
@@ -5,9 +5,11 @@ local Controllers = script.Parent:WaitForChild("Controllers")
 local InteractionController = require(Controllers:WaitForChild("InteractionController"))
 local PrototypeUIController = require(Controllers:WaitForChild("PrototypeUIController"))
 local CameraProtectionController = require(Controllers:WaitForChild("CameraProtectionController"))
+local FeelFeedbackController = require(Controllers:WaitForChild("FeelFeedbackController"))
 
 PrototypeUIController.Start()
 InteractionController.Start(PrototypeUIController)
 CameraProtectionController.Start()
+FeelFeedbackController.Start()
 
-print("[ONE TRIP] M1 client loaded")
+print("[ONE TRIP] M1.1 client loaded")
