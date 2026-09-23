682a4e241017f969e0d4305a3b57bbfe3756195e
hgvghvgjmvgj <130692440+hgvghvgjmvgj@users.noreply.github.com>
2026-09-23 04:47:34 +0300

    Correction: update client milestone banner

--- src/client/Main.client.lua
+++ src/client/Main.client.lua
@@ -16,4 +16,4 @@ CameraProtectionController.Start()
 FeelFeedbackController.Start()
 BayController.Start()
 
-print("[ONE TRIP] M3 SELL vs KEEP economy client loaded")
+print("[ONE TRIP] M3 correction economy + warehouse client loaded")
