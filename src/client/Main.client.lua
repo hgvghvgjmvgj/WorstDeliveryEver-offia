05ad966b23b85d5c8c4cd921865e2c6233752c68
hgvghvgjmvgj <130692440+hgvghvgjmvgj@users.noreply.github.com>
2026-09-24 03:42:41 +0300

    M6A.1: update client milestone label

--- src/client/Main.client.lua
+++ src/client/Main.client.lua
@@ -24,4 +24,4 @@ CameraProtectionController.Start()
 FeelFeedbackController.Start()
 BayController.Start()
 
-print("[ONE TRIP] M6A macro layout challenge client loaded")
+print("[ONE TRIP] M6A.1 shared warehouse runway client loaded")
