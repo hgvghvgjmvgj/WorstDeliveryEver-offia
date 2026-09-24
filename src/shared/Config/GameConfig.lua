06212f4bb33c1e86afe9b407ae5bad494f4d71e9
hgvghvgjmvgj <130692440+hgvghvgjmvgj@users.noreply.github.com>
2026-09-24 05:52:49 +0300

    M6A.3 economy: label corrected balance build

--- src/shared/Config/GameConfig.lua
+++ src/shared/Config/GameConfig.lua
@@ -2,7 +2,7 @@
 
 return table.freeze({
 	GameName = "ONE TRIP",
-	PrototypeVersion = "M6A.3-ONBOARDING+RIG-CLEARANCE+SPEED",
+	PrototypeVersion = "M6A.3-ONBOARDING+RIG-CLEARANCE+SPEED+ECONOMY",
 
 	MaxPlayersTarget = 12,
 
