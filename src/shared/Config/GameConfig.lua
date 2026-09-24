f984e7b3db8219a2ec58bcd82229607c597e8200
hgvghvgjmvgj <130692440+hgvghvgjmvgj@users.noreply.github.com>
2026-09-24 07:44:59 +0300

    M6C: update prototype milestone label

--- src/shared/Config/GameConfig.lua
+++ src/shared/Config/GameConfig.lua
@@ -2,7 +2,7 @@
 
 return table.freeze({
 	GameName = "ONE TRIP",
-	PrototypeVersion = "M6B-PROGRESSIVE-WAREHOUSE-WORLD-PASS",
+	PrototypeVersion = "M6C-CARGO-BASE+MODULAR-RARITY",
 
 	MaxPlayersTarget = 12,
 
